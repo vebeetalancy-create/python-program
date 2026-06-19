5d09aceed7e005ef37093f9bb19ac6ebb0dd9531
vebeetalancy-create <vebeetalancy@gmail.com>
2026-06-19 20:44:56 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,16 @@
+#numeric operations
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#arithmetic operations
+print("Addition =", num1 + num2)
+print("Subtraction =", num1 - num2)
+print("Multiplication =", num1 * num2)
+print("Division =", num1 / num2)
+print("Exponent =", num1 ** num2)
+print("Floor Division =", num1 // num2)
+print("Modulus =", num1 % num2)
+
+# Concatenate the inputs as strings
+str_concat = num1 + num2
+print("Concatenated string =", str_concat)
